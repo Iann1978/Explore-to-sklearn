e906a580f37d782ac6e270e49acbe2c3f18e3c5f
Iann Dang <iann1978@163.com>
2021-04-07 10:08:14 +0800

    * (book/NB) fixed a miner bug.

--- book_of_leiming/4-NaiveBayes.py
+++ book_of_leiming/4-NaiveBayes.py
@@ -9,7 +9,7 @@ from sklearn.naive_bayes import GaussianNB
 def make_meshgrid(x, y, h=.02):
     x_min, x_max = x.min() - 1, x.max() + 1
     y_min, y_max = y.min() - 1, y.max() + 1
-    xx, yy = np.meshgrid(np.arange(x_min, y_max, h),
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
                          np.arange(y_min, y_max, h))
     return xx, yy
 
